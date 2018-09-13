6d99f471c5ea6143c8e7062c33a1a0b51f3c3427
wadi <36459068+Wadibenarbia@users.noreply.github.com>
2018-09-13 15:15:25 +0200

    Routes finales

--- jules/src/routes.php
+++ jules/src/routes.php
@@ -8,17 +8,47 @@ require '../vendor/autoload.php';
 include ("Modal/MySQL.php");
 include_once "Get_Database/Authentification.php";
     
-    
+$app->POST('/', function (Request $data, Response $response, array $args) use ($app) {
+    // Render index view
+    $response = array("reponse" => 0);
+    $response = json_encode($response);
+    return $response;
+});   
+
 $app->POST('/Register', function (Request $request, Response $response, array $args) use ($app) {
     // Render index view
+    $data = $request->getBody();
+    $data = json_decode($data, true);
     $Authentification = new Authentification();
-    $Authentification->Get_Request($request);
+    $response = $Authentification->Get_Request($data);
+    return json_encode($response);
 });
 
 $app->POST('/Login', function (Request $request, Response $response, array $args) use ($app) {
-    // Render index view
+    
+    $data = $request->getBody();
+    $data = json_decode($data, true);
     $Authentification = new Authentification();
-    $Authentification->Get_Login($request);
+    $response = $Authentification->Get_Login($data);
+    return json_encode($response);
+});
+
+$app->POST('/ForgotPassword', function (Request $request, Response $response, array $args) use ($app) {
+    
+    $data = $request->getBody();
+    $data = json_decode($data, true);
+    $Authentification = new Authentification();
+    $response = $Authentification->Get_Password($data);
+    return json_encode($response);
+});
+
+$app->POST('/Infos', function (Request $request, Response $response, array $args) use ($app) {
+    
+    $data = $request->getBody();
+    $data = json_decode($data, true);
+    $Authentification = new Authentification();
+    $response = $Authentification->Get_Infos($data);
+    return json_encode($response);
 });
  
     
